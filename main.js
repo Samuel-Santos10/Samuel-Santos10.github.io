95b6c2b9071f0f1ef05f95d567b6050248064e05
julio alberto rodriguez pineda <jarp69721@gmail.com>
2021-03-07 15:48:20 -0600

    hoy si se guardo

---
+++ main.js
@@ -0,0 +1,46 @@
+var generarIdUnicoDesdeFecha=()=>{
+    let fecha = new Date();//03/02/2021
+    return Math.floor(fecha.getTime()/1000).toString(16);
+}, db;
+var appVue = new Vue({
+    el:'#appSistema',
+    data:{
+        forms:{
+            'clientes':{mostrar:false},
+        }
+    },
+    methods:{
+        abrirBd(){
+            let indexDb = indexedDB.open('db_sistema_agua_potable',1);
+            indexDb.onupgradeneeded=event=>{
+                let req=event.target.result,
+
+                    tblregistro = req.createObjectStore('tblclientes',{keyPath:'idCliente'})
+
+
+                tblregistro.createIndex('idCliente','idCliente',{unique:true});
+                tblregistro.createIndex('codigo','codigo',{unique:false});
+
+
+            };
+            indexDb.onsuccess = evt=>{
+                db=evt.target.result;
+            };
+            indexDb.onerror=e=>{
+                console.log("Error al conectar a la BD", e);
+            };
+        }
+    },
+    created(){
+        this.abrirBd();
+    }
+});
+
+document.addEventListener("DOMContentLoaded",event=>{
+    let el = document.querySelectorAll(".mostrar").forEach( (element, index)=>{
+        element.addEventListener("click",evt=>{
+            appVue.forms[evt.target.dataset.form].mostrar = true;
+            appVue.$refs[evt.target.dataset.form].obtenerDatos();
+        });
+    } );
+});
